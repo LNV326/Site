d3b2784cb98df996642cf63ba892ac0121f400b9
lnv326 <lnv326@gmail.com>
2015-06-30 00:58:45 +0300

    Files Template (final part)

--- SITE/src/Controller/FilesControllerDB.php
+++ SITE/src/Controller/FilesControllerDB.php
@@ -7,62 +7,7 @@ class FilesControllerDB extends AbstractSiteController {
 	
 	protected $_templateEngine = 'purePHP';
 	protected $_templateName = 'FilesTemplate.php';
-	
-	private function pages($subcat,$pages) {
-		if (isset($this->_nfs->input['subcat'])){
-			$more='&subcat='.$this->_nfs->input['subcat'];
-		}
-		if ($this->_nfs->input[sort]=='name') {
-			$sort_type='&sort=name';
-		}
-		if ($pages>1){
-			$pages_rul=$this->_lang[page].': [';
-			for ($i=0; $i<$pages; $i++){
-				$p=$i+1;
-				if ($i == $this->_nfs->input['p']){
-					$text='<font style="color: orange">'.$p.'</font>';
-				} else{
-					$text='<b>'.$p.'</b>';
-				}
-				if ($i>0) {
-					$pages_rul.='|<a href="index.php?page=files'.$more.'&p='.$i.$sort_type.'">'.$text.'</a>';
-				} else {
-					$pages_rul.='<a href="index.php?page=files'.$more.$sort_type.'">'.$text.'</a>';
-				}
-			}
-			$pages_rul.=']';
-		} else {
-			$pages_rul.='<b>'.$this->_conf[site_name].'</b>';
-		}
-		return $pages_rul;
-	}
-	
-	//Формирования нафигационной ссылки
-	private function cat_links($ids) {
-		$nav='<img src="'.$lil.'" border=0> ';
-		$this->_DB->query("SELECT * FROM s_files_cat WHERE id='".$ids."';");
-		$cat_row = $this->_DB->fetch_row();
-		if (isset($cat_row[id])){
-			$links=$nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files&cat='.$cat_row[id].'" title="Перейти">'.$cat_row[name].'</a>';
-		}else{
-			$links=$nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' Ошибка';
-		}
-		return $links;
-	}
-	
-	//Формирования нафигационной ссылки
-	private function sub_links($ids) { 
-		$nav='<img src="'.$lil.'" border=0> ';
-		$this->_DB->query("SELECT s.name as s_name, s.cat_id, c.name as c_name FROM s_files_subcat s LEFT JOIN s_files_cat c ON (s.cat_id=c.id) WHERE s.id='".$ids."';");
-		$row = $this->_DB->fetch_row();
-		if (isset($row[c_name])){
-			$links=$nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files&cat='.$row[cat_id].'" title="Перейти">'.$row[c_name].'</a> '.$nav.' <a href="index.php?page=files&subcat='.$ids.'" title="Перейти">'.$row[s_name].'</a>';
-		}else{
-			$links=$nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files" title="Перейти">Ошибка</a>';
-		}
-		return $links;
-	}
-	
+		
 	//Вывод подкатегории
 	private function subcat_list_for_type($sp,$ep,$scat,$typ) {
 		$files = array();
@@ -74,44 +19,17 @@ class FilesControllerDB extends AbstractSiteController {
 			}
 			$this->_DB->query("SELECT * FROM s_files_db WHERE subcat_id=".$scat." ORDER BY ".$sort_type." LIMIT $sp , $ep");			
 			while ($row = $this->_DB->fetch_row()) {
-				// TODO
 				//Авторство
-				if ($row[autor]==''){
-					$autor='Неизвестен';
-				} else {
-					$autor=$row[autor];
-				}
+				if ($row[autor]=='')
+					$row[autor]='Неизвестен';
 				//Проверка рисунка
 				if ($row[link]<>'1') {
-					if (($row[img]=='') or (!file_exists($this->_conf[images_path].$row[img]))) {
-						$row[img]='/style/'.$this->_style_id.'/img/no_image.gif';
-					}
-					if ($row[img_big]<>'') {
-						$linktobig="<p align=center><a href='http://images.".$this->_conf[site_url]."/".$row[img_big]."' target='_blank'>Увеличить</a></p>";
-					} else {
-						$linktobig="";
-					}
-				} else {
-					$linktobig="<p align=center><a href='http://images.".$this->_conf[site_url]."/".$row[img_big]."' target='_blank'>Увеличить</a></p>";
-				}
-				echo "<tr style='height:15pt'>\n";
-				echo "<td style='width:25%;border:solid windowtext 1.0pt'><p align=center>Закачек: ".$row[count]."</p></td>\n";
-				if ($row[link]<>'1') {
 					$size = null;
-					$size="Размер: ".get_size($row[url],$size);
-					$file_name=file_name($row[name]);
-					echo "<td style='width:48%;border:solid windowtext 1.0pt'><p align=center><b><a href='/download.php?go=".$row[id]."' title=Скачать>".$this->_nfs->unconvert_html($file_name)."</a></b></p></td>\n";
-					echo "<td style='width:25%;border:solid windowtext 1.0pt'><p align=center>".$size."</p></td>\n";
+					$row[size]="Размер: ".get_size($row[url],$size);
+					$row[file_name]=file_name($row[name]);
 				} else {
-					$file_name=$row[name];
-					echo "<td style='width:73%;border:solid windowtext 1.0pt' colspan=2><p align=center><b><a href='/download.php?go=".$row[id]."' title=Скачать>".$this->_nfs->unconvert_html($file_name)."</a></b></p></td>\n";
+					$row[file_name]=$row[name];
 				}
-				echo "</tr>\n";
-				echo "<tr style='height:15pt'>\n";
-				echo "<td style='border:solid windowtext 1.0pt'><p class=normal><img src='http://images.".$this->_conf[site_url]."/".$row[img]."' border=0></p>".$linktobig."</td>\n";
-				echo "<td colspan=2 style='border:solid windowtext 1.0pt'><p class=normal>".$this->_nfs->unconvert_html($row[description])."<br>Автор: <b>".$this->_nfs->unconvert_html($autor)."</b></p></td>\n";
-				echo "</tr>\n";
-				echo "<tr style='height:1pt;background:#151515'><td colspan=3 style='border:solid windowtext 1.0pt'><p class=normal style='font-size:1pt'>&nbsp;</p></td></tr>\n";
 				$files[] = $row;
 			}
 		} else {
@@ -131,57 +49,41 @@ class FilesControllerDB extends AbstractSiteController {
 		}
 		return $files;
 	}
-	
-// 	//Вывод списка подкатегорий по типам (0-наверху/1-внизу)
-// 	private function cat_subcat_list($cat,$type) { 
-// 		$subrez=$this->_DB->query("SELECT * FROM s_files_subcat WHERE cat_id='".$cat."' and type='".$type."' ORDER BY poz ASC");
-// 		while ($row = $this->_DB->fetch_row($subrez)) {
-// 			$vse = null;
-// 			if (($vse<>1) and ($type==1)){
-// 				echo "<tr style='height:15pt'><td colspan=3 style='border:solid windowtext 1.0pt'><p align=center>Другие файлы</p></td></tr>\n";
-// 				echo "<tr style='height:1pt;background:#151515'><td colspan=3 style='border:solid windowtext 1.0pt'><p style='font-size:1pt'>&nbsp;</p></td></tr>\n";
-// 				$vse=1;
-// 			}
-// 			echo "<tr style='height:15pt'>\n";
-// 			echo "<td colspan=2 style='width:80%;border:solid windowtext 1.0pt'><p class=normal>Архив: <b><a href='index.php?page=files&subcat=".$row[id]."' title=Перейти>".$this->_nfs->unconvert_html($row[name])."</a></b></p></td>\n";
-// 			echo "<td colspan=1 style='width:20%;border:solid windowtext 1.0pt'><p align=center>Файлов: ".$row[files_cnt]."</p></td>\n";
-// 			echo "</tr>\n";
-// 			echo "<tr style='height:15pt'><td colspan=3 style='border:solid windowtext 1.0pt'><p class=normal>".$this->_nfs->unconvert_html($row[info])."</p></td></tr>\n";
-// 			echo "<tr style='height:1pt;background:#151515'><td colspan=3 style='border:solid windowtext 1.0pt'><p style='font-size:1pt'>&nbsp;</p></td></tr>\n";
-// 		}
-// 	}
-	
+		
 	//Вывод списка подкатегорий
-	private function subcat_list($num,$subcat) {
+	private function subcat_list($num,$subcat_id) {
 		//Подсчёт кол-ва файлов в категории!
-		$this->_DB->query("SELECT count(*) FROM s_files_db WHERE subcat_id='".$subcat."'");
-		$filesCount = $this->_DB->fetch_row()['count'];
+		$this->_DB->query("SELECT count(*) as cnt FROM s_files_db WHERE subcat_id='".$subcat_id."'");
+		$filesCount = $this->_DB->fetch_row()['cnt'];
 		//округляем и получаем кол-во страниц
-		$pages=ceil($filesCount/$num);
+		
 		$p=intval($this->_nfs->input['p']);
 		$start=$p*$num;
 		//Получение типа категории
-		$this->_DB->query("SELECT * FROM s_files_subcat WHERE id='".$subcat."';");
+		$this->_DB->query("SELECT s.name as s_name, s.cat_id, c.name as c_name, s.id, s.type FROM s_files_subcat s LEFT JOIN s_files_cat c ON (s.cat_id=c.id) WHERE s.id='".$subcat_id."';");
 		$row_subcat = $this->_DB->fetch_row();
 		$type = $row_subcat[type];
 		
-		$pages_rul=$this->pages($subcat,$pages);
-		$links=$this->sub_links($subcat);
-		$files = $this->subcat_list_for_type($start,$num,$subcat,$type);
+// 		$pages_rul=$this->pages($subcat_id,$pages);
+// 		$links=$this->sub_links($subcat);
+		$files = $this->subcat_list_for_type($start,$num,$subcat_id,$type);
 		return array(
 				'page' => 'subcatList',
-				'subcatId' => $this->_nfs->input['subcat'],
+				'subcatId' => $subcat_id,
 				'pageNum' => $p,
+				'subcat' => $row_subcat,
 				'subcatType' => $type,
 				'files' => $files,
-				'filesCount' => $filesCount
+				'filesCount' => $filesCount,
+				'num' => $num
 		);
 	}
 	
 	//Вывод категории
 	private function cat_list($cat) {
-		$links=$this->cat_links($cat);
-
+// 		$links=$this->cat_links($cat);
+		$this->_DB->query("SELECT * FROM s_files_cat WHERE id='".$cat."';");
+		$catVal = $this->_DB->fetch_row();
 		//Строка с id подкатегорий в категории
 		$line="-1";
 		$subcats = array();
@@ -196,6 +98,7 @@ class FilesControllerDB extends AbstractSiteController {
 		
 		return array(
 				'page' => 'catList',
+				'cat' => $catVal,
 				'subcats' => $subcats,
 				'filesCount' => isset($row[count]) ? $row[count] : 0
 		);
--- SITE/src/Template/1/Controller/FilesTemplate.php
+++ SITE/src/Template/1/Controller/FilesTemplate.php
@@ -1,7 +1,49 @@
+
+<?php
+function pages($subcat,$pages) {
+	global $nfs, $lang, $conf;
+	if (isset($nfs->input['subcat'])){
+		$more='&subcat='.$nfs->input['subcat'];
+	}
+	if ($nfs->input[sort]=='name') {
+		$sort_type='&sort=name';
+	}
+	if ($pages>1){
+		$pages_rul=$lang[page].': [';
+		for ($i=0; $i<$pages; $i++){
+			$p=$i+1;
+			if ($i == $nfs->input['p']){
+				$text='<font style="color: orange">'.$p.'</font>';
+			} else{
+				$text='<b>'.$p.'</b>';
+			}
+			if ($i>0) {
+				$pages_rul.='|<a href="index.php?page=files'.$more.'&p='.$i.$sort_type.'">'.$text.'</a>';
+			} else {
+				$pages_rul.='<a href="index.php?page=files'.$more.$sort_type.'">'.$text.'</a>';
+			}
+		}
+		$pages_rul.=']';
+	} else {
+		$pages_rul.='<b>'.$conf[site_name].'</b>';
+	}
+	return $pages_rul;
+}
+
+$nav = '<img src="'.$lil.'" border=0> ';
+if (isset($out['subcat'])) {
+	$links = $nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files&cat='.$out[subcat][cat_id].'" title="Перейти">'.$out[subcat][c_name].'</a> '.$nav.' <a href="index.php?page=files&subcat='.$out[subcat][id].'" title="Перейти">'.$out[subcat][s_name].'</a>';
+} elseif (isset($out['cat'])) {
+	$links = $nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files&cat='.$out[cat][id].'" title="Перейти">'.$out[cat][name].'</a>';
+} else {
+	$links = $nav.'<a href="index.php?page=files" title="Перейти">Файловый архив</a> '.$nav.' <a href="index.php?page=files" title="Перейти">Ошибка</a>';
+}?>
+
+
 <?php switch($out['page']) { ?>
 <?php case 'subcatList' : {?>
 		<table class=table align=center border=0 cellspacing=0 style='width:98%'>
-		<tr style='height:10pt'><td colspan=3 style='border:solid windowtext 1.0pt'><p class=normal>".$links."</p></td></tr>
+		<tr style='height:10pt'><td colspan=3 style='border:solid windowtext 1.0pt'><p class=normal><?php echo $links;?></p></td></tr>
 		<tr style='height:3pt;background:#121212'><td colspan=3 style='border:solid windowtext 1.0pt'><p style='font-size:3pt'>&nbsp;</p></td></tr>
 <?php if ($out['filesCount'] == 0) {?>
 			<tr style='height:15pt'>
@@ -12,7 +54,33 @@
 			</tr>
 <?php }?>		
 <?php if ($out['subcatType'] == 1) {?>
-<?php foreach($out['files'] as $row) { //TODO?>
+<?php foreach($out['files'] as $row) { 
+				if ($row[link]<>'1') {
+					if (($row[img]=='') or (!file_exists($this->_conf[images_path].$row[img]))) {
+						$row[img]='/style/'.$this->_style_id.'/img/no_image.gif';
+					}
+					if ($row[img_big]<>'') {
+						$linktobig="<p align=center><a href='http://images.".$this->_conf[site_url]."/".$row[img_big]."' target='_blank'>Увеличить</a></p>";
+					} else {
+						$linktobig="";
+					}
+				} else {
+					$linktobig="<p align=center><a href='http://images.".$this->_conf[site_url]."/".$row[img_big]."' target='_blank'>Увеличить</a></p>";
+				}?>
+				<tr style='height:15pt'>
+				<td style='width:25%;border:solid windowtext 1.0pt'><p align=center>Закачек: <?php echo $row[count];?></p></td>
+				<?php if ($row[link]<>'1') {?>
+					<td style='width:48%;border:solid windowtext 1.0pt'><p align=center><b><a href='/download.php?go=<?php echo $row[id];?>' title=Скачать><?php echo $this->_nfs->unconvert_html($row[file_name]);?></a></b></p></td>
+					<td style='width:25%;border:solid windowtext 1.0pt'><p align=center><?php echo $row[size];?></p></td>
+				<?php } else {?>
+					<td style='width:73%;border:solid windowtext 1.0pt' colspan=2><p align=center><b><a href='/download.php?go=<?php echo $row[id];?>' title=Скачать><?php echo $this->_nfs->unconvert_html($row[file_name]);?></a></b></p></td>
+				<?php }?>
+				</tr>
+				<tr style='height:15pt'>
+				<td style='border:solid windowtext 1.0pt'><p class=normal><img src='http://images.<?php echo $this->_conf[site_url]."/".$row[img];?>' border=0></p><?php echo $linktobig;?></td>
+				<td colspan=2 style='border:solid windowtext 1.0pt'><p class=normal><?php echo $this->_nfs->unconvert_html($row[description]);?><br>Автор: <b><?php echo $this->_nfs->unconvert_html($row[autor]);?></b></p></td>
+				</tr>
+				<tr style='height:1pt;background:#151515'><td colspan=3 style='border:solid windowtext 1.0pt'><p class=normal style='font-size:1pt'>&nbsp;</p></td></tr>
 <?php }?>
 <?php } else {?>
 <?php foreach($out['files'] as $row) {?>
@@ -29,7 +97,7 @@
 				</tr>
 <?php }?>
 <?php }?>
-		<tr style='height:10pt'><td colspan=3 style='border:solid windowtext 1.0pt'><div align='left' style='float:left'><p class=normal>".$pages_rul."</p></div><div align='right'><p class=normal style='text-align:right'>Порядок: 
+		<tr style='height:10pt'><td colspan=3 style='border:solid windowtext 1.0pt'><div align='left' style='float:left'><p class=normal><?php $pages=ceil($out['filesCount']/$out['num']); echo pages($out['subcatId'], $pages);?></p></div><div align='right'><p class=normal style='text-align:right'>Порядок: 
 		<?php
 		//Ссылки для сортировки
 		$l1n='123';
@@ -49,7 +117,7 @@
 <?php case 'catList' : {?>
 <table class=table align=center border=0 cellspacing=0 style='width: 98%'>
 	<tr style='height: 10pt'>
-		<td colspan=3 style='border: solid windowtext 1.0pt'><p class=normal>".$links."</p></td>
+		<td colspan=3 style='border: solid windowtext 1.0pt'><p class=normal><?php echo $links;?></p></td>
 	</tr>
 	<tr style='height: 3pt; background: #121212'>
 		<td colspan=3 style='border: solid windowtext 1.0pt'><p class=normal style='font-size: 3pt'>&nbsp;</p></td>
