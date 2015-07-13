4847fb7868d41384cb80046e991804af70d172a5
lnv326 <lnv326@gmail.com>
2015-07-13 17:16:41 +0300

    Add some Utils (Debug, Redirect)

---
+++ SITE/src/Utils/Debug.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Utils;
+
+/**
+ * Class for getting operation time with milliseconds
+ * @author NLukyanov (LNV)
+ *
+ */
+abstract class Debug {
+	
+	static protected $starttime;
+	
+    public function startTimer() {
+        $mtime = microtime ();
+        $mtime = explode (' ', $mtime);
+        $mtime = $mtime[1] + $mtime[0];
+        self::$starttime = $mtime;
+    }
+    
+    public function endTimer() {
+        $mtime = microtime ();
+        $mtime = explode (' ', $mtime);
+        $mtime = $mtime[1] + $mtime[0];
+        $endtime = $mtime;
+        $totaltime = round (($endtime - self::$starttime), 5);
+        return $totaltime;
+    }
+}
